532a534b8e6c0cb049e2ce1af8ef2c886408222b
AlexP <itachilite@gmail.com>
2019-05-22 15:28:14 +0300

    repair_addEmp

--- client/src/components/Employees/AddEmployee.js
+++ client/src/components/Employees/AddEmployee.js
@@ -6,81 +6,16 @@ import { connect } from 'react-redux';
 //import classnames from "classnames";
 import { registerEmployee } from '../../redux/actions/authActions';
 import FormContainer from '../../services/FormContainer';
+import { inputs } from '../../services/dataInputs';
 
 class AddEmployee extends Component {
   constructor(props) {
     super(props);
-/* for what
-    this.state = {
-      FirstName: '',
-      LastName: '',
-      Password: '',
-      Password2: '',
-      Phone: '',
-      Birthday: '',
-      Adress: '',
-      Position: '',
-      SalaryChange: '',
-      errors: {}
-    };
-*/
+
     this.headerForm = 'Регистрация';
     this.submitLabel = 'Зарегистрировать';
-
-    this.dataInput = [
-      {
-        type: 'text',
-        label: 'Имя',
-        name: 'FirstName',
-        helperText: ''
-      },
-      {
-        type: 'text',
-        label: 'Фамилия',
-        name: 'LastName',
-        helperText: ''
-      },
-      {
-        type: 'number',
-        label: 'Телефон',
-        name: 'Phone',
-        helperText: ''
-      },
-
-      {
-        type: 'password',
-        label: 'Пароль',
-        name: 'Password',
-        helperText: ''
-      },
-      {
-        type: 'password',
-        label: 'Повторить пароль',
-        name: 'Password2'
-      },
-      {
-        type: 'date',
-        label: 'День рождения',
-        name: 'Birthday'
-      },
-      {
-        type: 'text',
-        label: 'Домашний адрес (Город, улица, дом, квартира)',
-        name: 'Adress'
-      },
-      {
-        type: 'text',
-        label: 'Должность сотрудника',
-        name: 'Position'
-      },
-      {
-        type: 'number',
-        label: 'Оклад сотрудника за одну смену',
-        name: 'SalaryChange'
-      }
-    ];
+    this.dataInput = inputs.employeeInputs;
   }
-
   onSubmit = values => this.props.registerEmployee(values);
 
   render() {
